52c9e39f6ed3bd7ac8151dcf20a40131e8d53aef
Siddharth Singh <95075464+siddharthsinghvats@users.noreply.github.com>
2023-01-06 13:52:19 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import "../index.css";
-import profile from "../images/prof.jpeg";
 import tech from "../images/tech.png";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
