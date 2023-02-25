30da454dff2e449859d666d7d608a9567a194d90
Siddharth Singh <95075464+siddharthsinghvats@users.noreply.github.com>
2023-02-25 10:09:39 +0530

    update leetcode username

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -15,7 +15,7 @@ const Code = () => {
   const [cfuser,setCfuser] =useState({});
   const [loading,setLoading]=useState(true);
   useEffect(()=>{
-    fetch("https://leetcode-stats-api.herokuapp.com/it_is_tough")
+    fetch("https://leetcode-stats-api.herokuapp.com/life_is_lie")
     .then(res => res.json())
     .then(
       (result) => {
